922368675feb7a3ed9884596e656b86b406b3ef4
khantkyawkhaung <khantkyawkhaung288@gmail.com>
2021-11-14 15:14:13 +0630

    :package: Debian package

---
+++ dist/debian/librmg/src/config/rmg/config.h
@@ -0,0 +1,16 @@
+#ifndef __RMG_CONFIG_H__
+#define __RMG_CONFIG_H__
+
+/**
+ * @brief Constant string representing RMGraphics version
+ */
+#define RMG_VERSION_STRING "1.0.0"
+
+/**
+ * @brief Path to shared static resources that comes together with RMGraphics
+ */
+#ifndef RMG_RESOURCE_PATH
+#define RMG_RESOURCE_PATH "/usr/share/rmg"
+#endif
+
+#endif
