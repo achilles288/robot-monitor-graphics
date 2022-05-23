dc6cd898090e687b3d4dd72ed56a7c19ec67d408
khantkyawkhaung <khantkyawkhaung288@gmail.com>
2022-05-24 04:29:43 +0630

    :wrench: Test configurations

---
+++ test/test.cpp
@@ -0,0 +1,10 @@
+#include "testconf.h"
+
+#include <iostream>
+
+
+int main() {
+    std::cout << "Test run" << std::endl;
+    std::cout << "RMGTEST_RESOURCE_PATH " << RMGTEST_RESOURCE_PATH << std::endl;
+    std::cout << "RMGTEST_OUTPUT_PATH " << RMGTEST_RESOURCE_PATH << std::endl;
+}
